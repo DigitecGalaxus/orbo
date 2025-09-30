c430992dca1c955f97e056f81a333046dbff5549
Jan Nicklas <j.nicklas@me.com>
2025-09-30 11:14:50 +0200

    add ssr for example for hydrateRoot

--- examples/base/src/_app.tsx
+++ examples/base/src/_app.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { hydrateRoot } from "react-dom/client";
 import { GlobalStateProvider } from "orbo";
 import Page from "./page";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <GlobalStateProvider initialValues={{}}>
-      <Page />
-    </GlobalStateProvider>
-  </React.StrictMode>,
-);
+function App() {
+  return (
+    <React.StrictMode>
+      <GlobalStateProvider initialValues={{}}>
+        <Page />
+      </GlobalStateProvider>
+    </React.StrictMode>
+  );
+}
+
+// For SSR template
+export default App;
+
+// For client-side hydration
+if (typeof window !== "undefined") {
+  hydrateRoot(document.getElementById("root")!, <App />);
+}
